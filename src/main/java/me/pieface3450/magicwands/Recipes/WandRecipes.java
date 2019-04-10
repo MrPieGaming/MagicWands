94767c4951858d0511f7ddf0d15a40bf31609b52
MrPieGaming <pieface3450@gmail.com>
2019-04-10 15:05:38 -0400

    more class optimizations, changed project version

---
+++ src/main/java/me/pieface3450/magicwands/Recipes/WandRecipes.java
@@ -0,0 +1,48 @@
+package me.pieface3450.magicwands.Recipes;
+
+import me.pieface3450.magicwands.Wands.FireballWandItem;
+import me.pieface3450.magicwands.Wands.LightningWandItem;
+import me.pieface3450.magicwands.Wands.TeleportWandItem;
+import org.bukkit.Material;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+
+public class WandRecipes {
+
+    public static Recipe fireballWandRecipe() {
+        ShapedRecipe fireballWandRecipe = new ShapedRecipe(FireballWandItem.fireballWandItem());
+        fireballWandRecipe.shape(
+                "#L#",
+                "#S#",
+                "#S#");
+        fireballWandRecipe.setIngredient('#', Material.AIR);
+        fireballWandRecipe.setIngredient('L', Material.LAVA_BUCKET);
+        fireballWandRecipe.setIngredient('S', Material.STICK);
+
+        return fireballWandRecipe;
+    }
+
+    public static Recipe tpWandRecipe() {
+        ShapedRecipe tpWandRecipe = new ShapedRecipe(TeleportWandItem.teleportWandItem());
+        tpWandRecipe.shape("#E#",
+                "#S#",
+                "#S#");
+        tpWandRecipe.setIngredient('#', Material.AIR);
+        tpWandRecipe.setIngredient('E', Material.ENDER_PEARL);
+        tpWandRecipe.setIngredient('S', Material.STICK);
+
+        return tpWandRecipe;
+    }
+
+    public static Recipe lightningWandRecipe() {
+        ShapedRecipe lightningWandRecipe = new ShapedRecipe(LightningWandItem.lightningWandItem());
+        lightningWandRecipe.shape("#N#",
+                "#S#",
+                "#S#");
+        lightningWandRecipe.setIngredient('#', Material.AIR);
+        lightningWandRecipe.setIngredient('N', Material.NETHER_STAR);
+        lightningWandRecipe.setIngredient('S', Material.STICK);
+
+        return lightningWandRecipe;
+    }
+}
